b7d93618ac01c292613b1c532f7fea8993790d75
madebytimo <t.schnaible@madebytimo.de>
2025-05-10 03:13:49 +0200

    fix permission problems if run as non-root

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -5,13 +5,17 @@ if [[ -z "$AI_BASE_URL" ]]; then
     export AI_BASE_URL="$AI_API_URL"
 fi
 
-chown --recursive user /media/user
+if [[ "$(id --user)" -eq 0 ]]; then
+    chown --recursive user /media/user
+    usermod --remove --groups "$USER_GROUPS" user
+    usermod --append --groups "$USER_GROUPS" user
 
-usermod --remove --groups "$USER_GROUPS" user
-usermod --append --groups "$USER_GROUPS" user
+    su user --command "XDG_RUNTIME_DIR=/run/user/$(id -u user) dockerd-rootless.sh &> /dev/null" &
+else
+    export "XDG_RUNTIME_DIR=/run/user/$(id -u)"
+    dockerd-rootless.sh &> /dev/null &
+fi
 
-echo "start docker deamon"
-
-su user --command "XDG_RUNTIME_DIR=/run/user/$(id -u user) dockerd-rootless.sh &> /dev/null" &
+disown -h
 
 exec "$@"
