31c82d0fd029f7249fff8ec8064dddda761e4332
madebytimo <t.schnaible@madebytimo.de>
2026-08-22 22:57:51 +0200

    write only to /etc/profile.d/ if root

--- files/entrypoint-development.sh
+++ files/entrypoint-development.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-if [[ "$(id --user)" -eq 0 ]]; then
+if [[ "$EUID" -eq 0 ]]; then
     usermod --groups "$USER_GROUPS" user
-fi
 
-cat <<EOF > /etc/profile.d/env-development.sh
+    cat <<EOF > /etc/profile.d/env-development.sh
 #!/usr/bin/env bash
 
 LANG="$LANG"
@@ -14,5 +13,6 @@ XDG_RUNTIME_DIR="/run/user/\$(id -u)"
 
 export DOCKER_HOST LANG XDG_RUNTIME_DIR
 EOF
+fi
 
 exec "$@"
